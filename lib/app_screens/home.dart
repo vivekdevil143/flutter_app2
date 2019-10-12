1412b4c547ca66e3ddff60fbf6bf8ce65af55d52
vivekdevil143 <vivekdevil143@gmail.com>
2019-10-12 18:59:54 +0530

    inital commit  12/10/2019

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,31 +1,117 @@
 import 'package:flutter/material.dart';
 
-
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Center(
-      child: Container(
-        alignment: Alignment.center,
-        color: Colors.deepPurple,
-     //   width: 200.0,
-      //  height: 200.0,
-        //margin: EdgeInsets.only(right:15.0,top: 15.0),
-        //padding: EdgeInsets.all(15.0),
-        child: Text(
-          "Flight",textDirection: TextDirection.ltr,
-        style: TextStyle(
-          decoration: TextDecoration.none,
-          fontSize: 75.0,
-          fontFamily: 'Raleway',
-            fontWeight: FontWeight.w700,
-          color: Colors.white
+        child: Container(
+            padding: EdgeInsets.only(left: 10.0, top: 40.0),
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            //   width: 200.0,
+            //  height: 200.0,
+            //margin: EdgeInsets.only(right:15.0,top: 15.0),
+            //padding: EdgeInsets.all(15.0),
+            child: Column(children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                    "Spice jet",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 35.0,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        fontStyle: FontStyle.italic),
+                  )),
+                  Expanded(
+                      child: Text(
+                    "From mumbai to delhi",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 35.0,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        fontStyle: FontStyle.italic),
+                  )),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                    "Airindia jet",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 35.0,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        fontStyle: FontStyle.italic),
+                  )),
+                  Expanded(
+                      child: Text(
+                    "From paris to delhi",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 35.0,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        fontStyle: FontStyle.italic),
+                  )),
+                ],
+              ),
+              FlightImageAsset(),
+              FlightBookRaisedButton()
+            ])));
+  }
+}
 
-          //fontStyle:FontStyle.italic
-        ),
-        ),
-      )
+class FlightImageAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/download.png');
+    Image image = Image(image: assetImage, width: 250.0, height: 250.0);
+    return Container(child: image);
+  }
+}
+
+class FlightBookRaisedButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      width: 250.0,
+      height: 50.0,
+      child: RaisedButton(
+          color: Colors.deepOrange,
+          child: Text(
+            "Book Your Flight",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontFamily: 'Raleway',
+            ),
+          ),
+          elevation: 6.0,
+          onPressed: () => BookFlight(context)),
     );
+  }
+
+  void BookFlight(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: Text("Flight Book Successfully"),
+      content: Text("Have a pleasant flight"),
+    );
+
+    showDialog(context: context, builder: (BuildContext context) => alertDialog);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,41 @@ void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Exploring UI widgets",
-    home: Home(),
+    //home: Home(),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Basic List view"),),
+       // body: getListView(),
+      )
   ));
 
+}
+
+
+Widget getListView(){
+
+  var listview=ListView(
+    children: <Widget>[
+
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+       subtitle: Text("Beautiful view "),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: (){
+          debugPrint("Landscape Tapped");
+        }
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_chromebook),
+        title: Text("Windows"),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Phone"),
+      ),
+      Text("Yet another element"),
+      Container(color: Colors.red,height: 50.0,)
+
+    ],
+  );
 }
